5bccc43f6bc67be3820d9408cfe8d1b5a3dad327
maytic <maytic@users.noreply.github.com>
2020-12-23 22:36:12 -0800

    first

---
+++ src/main/java/com/maytic/pokemondb/PokemondbWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.maytic.pokemondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemondbWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemondbWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maytic/pokemondb/PokemondbWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maytic.pokemondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemondbWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
